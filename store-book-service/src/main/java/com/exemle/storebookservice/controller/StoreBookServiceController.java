b78d8b75c4e8dcec906c976abae54d36cbcd73cd
redmi <Denis-de>
2023-12-24 20:04:24 +0300

    Commit3

--- store-book-service/src/main/java/com/exemle/storebookservice/controller/StoreBookServiceController.java
+++ store-book-service/src/main/java/com/exemle/storebookservice/controller/StoreBookServiceController.java
@@ -4,7 +4,7 @@ import com.exemle.storebookservice.model.RemotePropertyField;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestControlle
 public class StoreBookServiceController {
     private RemotePropertyField field;
 
