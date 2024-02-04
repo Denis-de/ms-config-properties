7693b23c09500c748dc75086074a36ca1df29b53
redmi <Denis-de>
2024-02-04 19:57:29 +0300

    Commit2

--- check-book-service/src/main/java/com/exemple/checkbookservice/controller/CheckBookServiceController.java
+++ check-book-service/src/main/java/com/exemple/checkbookservice/controller/CheckBookServiceController.java
@@ -1,19 +1,18 @@
 package com.exemple.checkbookservice.controller;
 
 import com.exemple.checkbookservice.model.RemotePropertyField;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*@RestController
+@RestController
+@AllArgsConstructor
 public class CheckBookServiceController {
     private RemotePropertyField field;
 
-    public CheckBookServiceController(RemotePropertyField field) {
-        this.field = field;
-    }
 
     @GetMapping(value = "/getRemoteField")
     public String getRemotePropertyField() {
         return "Data: " + field.getField();
     }
-}**/
+}
