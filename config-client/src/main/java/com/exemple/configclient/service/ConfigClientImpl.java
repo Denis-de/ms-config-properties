c4c78d7672a538f29f889ab750673dddb8273ef0
redmi <Denis-de>
2024-02-04 18:55:17 +0300

    Commit2

--- config-client/src/main/java/com/exemple/configclient/service/ConfigClientImpl.java
+++ config-client/src/main/java/com/exemple/configclient/service/ConfigClientImpl.java
@@ -2,6 +2,7 @@ package com.exemple.configclient.service;
 
 import com.exemple.configclient.connector.BookServiceConnector;
 import com.exemple.configclient.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class ConfigClientImpl implements ConfigClient {
     private final BookServiceConnector bookServiceConnector;
+
 
     public ConfigClientImpl(BookServiceConnector bookServiceConnector) {
         this.bookServiceConnector = bookServiceConnector;
